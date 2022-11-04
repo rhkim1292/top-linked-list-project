ae2c361cf41a9d53d4496de3306dcb2cb3bf3c49
Randy Kim <rhkim1292@gmail.com>
2022-11-04 02:24:05 -0700

    Implement the removeAt method in LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -10,10 +10,13 @@ class LinkedList {
       this._head = new Node(value);
       return;
     }
+
     let curr = this._head;
+
     while (curr.next) {
       curr = curr.next;
     }
+
     curr.next = new Node(value);
   }
 
@@ -22,6 +25,7 @@ class LinkedList {
       this._head = new Node(value);
       return;
     }
+
     const newHead = new Node(value, this._head);
     this._head = newHead;
   }
@@ -29,10 +33,12 @@ class LinkedList {
   size() {
     let counter = 0;
     let curr = this._head;
+
     while (curr) {
       counter++;
       curr = curr.next;
     }
+
     return counter;
   }
 
@@ -42,73 +48,107 @@ class LinkedList {
 
   tail() {
     let curr = this._head;
+
     while (curr.next) {
       curr = curr.next;
     }
+
     return curr;
   }
 
   at(index) {
     let curr = this._head;
+
     for (let i = 0; i < index; i++) {
       curr = curr.next;
       if (!curr) throw new Error('That index is out of bounds of the list!');
     }
+
     return curr;
   }
 
   pop() {
     let curr = this._head;
     let prev = null;
+
     while (curr.next) {
       prev = curr;
       curr = curr.next;
     }
+
     prev.next = null;
   }
 
   contains(value) {
     let curr = this._head;
+
     while (curr) {
       if (curr.val === value) return true;
       curr = curr.next;
     }
+
     return false;
   }
 
   find(value) {
     let curr = this._head;
+
     while (curr) {
       if (curr.val === value) return curr;
       curr = curr.next;
     }
+
     return curr;
   }
 
   toString() {
     let result = '';
     let curr = this._head;
+
     while (curr) {
       result += `( ${curr.val} ) -> `;
       curr = curr.next;
     }
+
     result += 'null';
+
     return result;
   }
 
   insertAt(value, index) {
     let curr = this._head;
     let prev = null;
-	if (index === this.size()) {
-		this.append(value);
-		return;
-	}
+
+    if (index === this.size()) {
+      this.append(value);
+      return;
+    }
+
     for (let i = 0; i < index; i++) {
       prev = curr;
       curr = curr.next;
       if (!curr) throw new Error('That index is out of bounds of the list!');
     }
+
     prev.next = new Node(value, curr);
+  }
+
+  removeAt(index) {
+    let curr = this._head;
+    let prev = null;
+
+    for (let i = 0; i < index; i++) {
+      prev = curr;
+      curr = curr.next;
+      if (!curr) throw new Error('That index is out of bounds of the list!');
+    }
+	
+	if (index === 0) {
+		this._head = this._head.next;
+		return;
+	}
+
+	prev.next = curr.next;
   }
 }
 
@@ -135,4 +175,12 @@ console.log(list.find(9));
 console.log(list.toString());
 console.log(list.size());
 list.insertAt(4, 3);
+console.log(list.toString());
+list.removeAt(0);
+console.log(list.toString());
+list.removeAt(0);
+console.log(list.toString());
+list.removeAt(1);
+console.log(list.toString());
+list.removeAt(1);
 console.log(list.toString());
