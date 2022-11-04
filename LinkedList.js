a0886b91413993795e0323ce7d0a02020ed23f5a
Randy Kim <rhkim1292@gmail.com>
2022-11-03 23:47:21 -0700

    Implement getter for the size&head of LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -1,13 +1,25 @@
 import Node from './Node.js';
 
 class LinkedList {
-    constructor(head = null) { this.head = head; }
+    constructor(head = null) { this._head = head; }
+    get size() {
+        if (this.head === 0) return 0;
+        let counter = 0;
+        let curr = this.head;
+        while (curr) {
+            counter++;
+            curr = curr.next;
+        }
+        return counter;
+    }
+    get head() { return this._head }
+
     append(value) {
-        if (!this.head) {
-            this.head = new Node(value);
+        if (!this._head) {
+            this._head = new Node(value);
             return;
         }
-        let curr = this.head;
+        let curr = this._head;
         while (curr.next) {
             curr = curr.next;
         }
@@ -15,19 +27,22 @@ class LinkedList {
     }
 
     prepend(value) {
-        if (!this.head) {
-            this.head = new Node(value);
+        if (!this._head) {
+            this._head = new Node(value);
             return;
         }
-        const newHead = new Node(value, this.head);
-        this.head = newHead;
+        const newHead = new Node(value, this._head);
+        this._head = newHead;
     }
 }
 
 const list = new LinkedList();
+const emptyList = new LinkedList();
 console.log(list);
 list.append(5);
 list.append(3);
 list.append(8);
 list.prepend(3);
 console.log(list.head);
+console.log(list.size);
+console.log(emptyList.size);
